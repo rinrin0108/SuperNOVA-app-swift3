dede6989f8ebb64ff29481d781fe67ac69a6993f
y-okada <y-okada@y-okada-no-MacBook-Air.local>
2017-01-17 15:59:11 +0900

    mod EncounterViewController

--- SuperNOVA-app/EncounterViewController.swift
+++ SuperNOVA-app/EncounterViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 
 class EncounterViewController: UIViewController {
     
-    
     @IBOutlet weak var photo_student: UIImageView!
     @IBOutlet weak var photo_teacher: UIImageView!
     @IBOutlet weak var name_student: UILabel!
@@ -38,81 +37,75 @@ class EncounterViewController: UIViewController {
         
         //教師
         UserAPI.getUser(appDelegate._partner,sync: true,
-                                  success:{
-                                    values in let closure = {
-                                        NSLog("---EncounterViewController UserAPI.getUser success");
-                                        // 通信は成功したが、エラーが返ってきた場合
-                                        if(API.isError(values)){
-                                            NSLog("---EncounterViewController UserAPI.getUser isError");
-                                            /**
-                                             * ストーリーボードをまたぐ時に値を渡すためのもの（Indicatorストーリーボードを作成する必要あり）
-                                             Indicator.windowClose()
-                                             */
-                                            AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
+            success:{
+                values in let closure = {
+                    NSLog("---EncounterViewController UserAPI.getUser success");
+                    // 通信は成功したが、エラーが返ってきた場合
+                    if(API.isError(values)){
+                        NSLog("---EncounterViewController UserAPI.getUser isError");
+                        /**
+                         *  ストーリーボードをまたぐ時に値を渡すためのもの（Indicatorストーリーボードを作成する必要あり）
+                            Indicator.windowClose()
+                         */
+                        AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
                                                 message: values["errorMessage"] as! String)
-                                            return
-                                        }
-                                        
-                                        
-                                        if values.isEmpty == true {
-                                            // 誰ともマッチしなかった場合
-                                            print("no result-----------")
-                                            ViewShowAnimation.changeViewWithIdentiferFromHome(self, toVC: "backToMap")
-                                            return
-                                        }
-                                        NSLog(values.debugDescription);
-                                        appDelegate._partnerimage = values["image"] as! String
-                                        appDelegate._partnerName = values["fullname"] as! String
-                                        self.photo_teacher.image =  API.downloadImage(appDelegate._partnerimage)
-                                        self.photo_teacher.layer.cornerRadius = self.photo_teacher.frame.size.width / 2
-                                        self.photo_teacher.clipsToBounds = true
-                                        self.photo_teacher.layer.borderColor = UIColor.orange.cgColor
-
-                                        self.name_teacher.text = appDelegate._partnerName
-                                        
-                                    }
-                                    // 通知の監視
-                                    if(!Thread.isMainThread){
-                                        NSLog("---EncounterViewController !NSThread.isMainThread() in success");
-                                        DispatchQueue.main.sync {
-                                            NSLog("---EncounterViewController dispatch_sync");
-                                            closure()
-                                        }
-                                    } else {
-                                        NSLog("---EncounterViewController dispatch_sync else");
-                                        // 恐らく実行されない
-                                        closure()
-                                    }
+                        return
+                    }
+                    
+                    if values.isEmpty == true {
+                        // 誰ともマッチしなかった場合
+                        print("no result-----------")
+                        ViewShowAnimation.changeViewWithIdentiferFromHome(self, toVC: "backToMap")
+                        return
+                    }
+                    
+                    NSLog(values.debugDescription);
+                    appDelegate._partnerimage = values["image"] as! String
+                    appDelegate._partnerName = values["fullname"] as! String
+                    self.photo_teacher.image =  API.downloadImage(appDelegate._partnerimage)
+                    self.photo_teacher.layer.cornerRadius = self.photo_teacher.frame.size.width / 2
+                    self.photo_teacher.clipsToBounds = true
+                    self.photo_teacher.layer.borderColor = UIColor.orange.cgColor
+                    self.name_teacher.text = appDelegate._partnerName
+                }
+                
+                // 通知の監視
+                if(!Thread.isMainThread){
+                    NSLog("---EncounterViewController !NSThread.isMainThread() in success");
+                    DispatchQueue.main.sync {
+                        NSLog("---EncounterViewController dispatch_sync");
+                        closure()
+                    }
+                } else {
+                        NSLog("---EncounterViewController dispatch_sync else");
+                        closure()   // 恐らく実行されない
+                }
                                     
             },
-                                  failed: {
-                                    id, message in let closure = {
-                                        NSLog("---EncounterViewController UserAPI.getUser failed");
-                                        // 失敗した場合エラー情報を表示
-                                        if(id == -2) {
-                                            AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
-                                                message: NSLocalizedString("MAX_FILE_SIZE_OVER", comment: ""));
-                                        } else {
-                                            AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
-                                                message: NSLocalizedString("ALERT_MESSAGE_NETWORK_ERROR", comment: ""));
-                                        }
-                                    }
-                                    // 通知の監視
-                                    if(!Thread.isMainThread){
-                                        NSLog("---EncounterViewController !NSThread.isMainThread() in failed");
-                                        DispatchQueue.main.sync {
-                                            NSLog("---EncounterViewController dispatch_sync");
-                                            closure()
-                                        }
-                                    } else {
-                                        NSLog("---EncounterViewController dispatch_sync else");
-                                        //恐らく実行されない
-                                        closure()
-                                    }
+            failed: {
+                id, message in let closure = {
+                    NSLog("---EncounterViewController UserAPI.getUser failed");
+                    // 失敗した場合エラー情報を表示
+                    if(id == -2) {
+                        AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),message: NSLocalizedString("MAX_FILE_SIZE_OVER", comment: ""));
+                    } else {
+                        AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),message: NSLocalizedString("ALERT_MESSAGE_NETWORK_ERROR", comment: ""));
+                    }
+                }
+                // 通知の監視
+                if(!Thread.isMainThread){
+                    NSLog("---EncounterViewController !NSThread.isMainThread() in failed");
+                    DispatchQueue.main.sync {
+                        NSLog("---EncounterViewController dispatch_sync");
+                        closure()
+                    }
+                } else {
+                        NSLog("---EncounterViewController dispatch_sync else");
+                        closure()   //恐らく実行されない
+                }
             }
         )
     }
-    
     
     var timerOn = false
     var nsTimer = Timer()
@@ -131,65 +124,59 @@ class EncounterViewController: UIViewController {
         
         let starttime :String! = dateFormatter.string(from: now)
         MergerAPI.updatePitchStarttime(appDelegate._id , starttime: starttime ,sync: true,
-                                       success:{
-                                        values in let closure = {
-                                            NSLog("---EncounterViewController MergerAPI.updatePitchStarttime success");
-                                            // 通信は成功したが、エラーが返ってきた場合
-                                            if(API.isError(values)){
-                                                NSLog("---EncounterViewController MergerAPI.updatePitchStarttime isError");
-                                                /**
-                                                 * ストーリーボードをまたぐ時に値を渡すためのもの（Indicatorストーリーボードを作成する必要あり）
-                                                 Indicator.windowClose()
-                                                 */
-                                                AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
-                                                    message: values["errorMessage"] as! String)
-                                                return
-                                            }
+            success:{
+                values in let closure = {
+                    NSLog("---EncounterViewController MergerAPI.updatePitchStarttime success");
+                    // 通信は成功したが、エラーが返ってきた場合
+                    if(API.isError(values)){
+                        NSLog("---EncounterViewController MergerAPI.updatePitchStarttime isError");
+                        /**
+                         *  ストーリーボードをまたぐ時に値を渡すためのもの（Indicatorストーリーボードを作成する必要あり）
+                            Indicator.windowClose()
+                         */
+                        AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),message: values["errorMessage"] as! String)
+                        return
+                    }
+                    NSLog(values.debugDescription);
                                             
-                                            NSLog(values.debugDescription);
-                                            
-                                        }
-                                        // 通知の監視
-                                        if(!Thread.isMainThread){
-                                            NSLog("---EncounterViewController !NSThread.isMainThread() in success");
-                                            DispatchQueue.main.sync {
-                                                closure()
-                                            }
-                                        } else {
-                                            NSLog("---EncounterViewController closure");
-                                            // 恐らく実行されない
-                                            closure()
-                                        }
-                                        
+                }
+                // 通知の監視
+                if(!Thread.isMainThread){
+                    NSLog("---EncounterViewController !NSThread.isMainThread() in success");
+                    DispatchQueue.main.sync {
+                        closure()
+                    }
+                } else {
+                    NSLog("---EncounterViewController closure");
+                    closure()   // 恐らく実行されない
+                }
             },
-                                       failed: {
-                                        id, message in let closure = {
-                                            NSLog("---EncounterViewController MergerAPI.updatePitchStarttime failed");
-                                            /**
-                                             * ストーリーボードをまたぐ時に値を渡すためのもの（Indicatorストーリーボードを作成する必要あり）
-                                             Indicator.windowClose()
-                                             */
-                                            // 失敗した場合エラー情報を表示
-                                            if(id == -2) {
-                                                AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
-                                                    message: NSLocalizedString("MAX_FILE_SIZE_OVER", comment: ""));
-                                            } else {
-                                                AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
-                                                    message: NSLocalizedString("ALERT_MESSAGE_NETWORK_ERROR", comment: ""));
-                                            }
-                                        }
-                                        // 通知の監視
-                                        if(!Thread.isMainThread){
-                                            NSLog("---EncounterViewController !NSThread.isMainThread() in failed");
-                                            DispatchQueue.main.sync {
-                                                NSLog("---EncounterViewController dispatch_sync");
-                                                closure()
-                                            }
-                                        } else {
-                                            NSLog("---EncounterViewController dispatch_sync else");
-                                            //恐らく実行されない
-                                            closure()
-                                        }
+            failed: {
+                id, message in let closure = {
+                    NSLog("---EncounterViewController MergerAPI.updatePitchStarttime failed");
+                    /**
+                     *  ストーリーボードをまたぐ時に値を渡すためのもの（Indicatorストーリーボードを作成する必要あり）
+                        Indicator.windowClose()
+                     */
+                    // 失敗した場合エラー情報を表示
+                    if(id == -2) {
+                        AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),message: NSLocalizedString("MAX_FILE_SIZE_OVER", comment: ""));
+                    } else {
+                        AlertUtil.alertError(self, title: NSLocalizedString("ALERT_TITLE_ERROR", comment: ""),
+                            message: NSLocalizedString("ALERT_MESSAGE_NETWORK_ERROR", comment: ""));
+                    }
+                }
+                // 通知の監視
+                if(!Thread.isMainThread){
+                    NSLog("---EncounterViewController !NSThread.isMainThread() in failed");
+                    DispatchQueue.main.sync {
+                        NSLog("---EncounterViewController dispatch_sync");
+                        closure()
+                    }
+                } else {
+                        NSLog("---EncounterViewController dispatch_sync else");
+                        closure()   //恐らく実行されない
+                }
             }
         )
         
